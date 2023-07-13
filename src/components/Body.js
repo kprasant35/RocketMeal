1b4bacabdd581e57829119fa5c623e778cc71191
kprasant35 <prasantkumar.bt19ece@pec.edu.in>
2023-07-13 20:55:22 +0530

    Added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,50 @@
 import {restaurantList} from "../constants.js"
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+const filteredRestaurants = (searchText) =>{
+    const restaurants = restaurantList.filter((restaurant) =>{
+        return restaurant?.data?.name?.toLowerCase().includes(searchText.toLowerCase());
+    });
+
+    return restaurants;
+}
 
 const Body = () =>{
+    const [searchText, setSearchText] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
+
     return (
-        <div className="restaurant-list">
-            {restaurantList.map((restaurant) =>{
-                
-                return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>;
-            })}
-        </div>
+        <> 
+            <div className="search-container">
+             <form action="" onSubmit={(e) => e.preventDefault()}>
+
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search a restaurant you want..."
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}   
+
+                    ></input>
+
+                <button
+                    className="search-btn"
+                    onClick={() => {
+                        const data = filteredRestaurants(searchText);
+                        setRestaurants(data);
+                    }}
+                > Search</button>
+              </form>
+            </div>
+
+            <div className="restaurant-list">
+                {restaurants.map((restaurant) =>{
+                    
+                    return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>;
+                })}
+            </div>
+        </>
     );
 }
 
